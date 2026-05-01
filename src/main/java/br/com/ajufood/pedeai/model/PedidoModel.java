ba706e669b080569fdd5e61b5f526b3dc99dd586
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-05-01 14:49:59 -0300

    feat: add PedidoModel

---
+++ src/main/java/br/com/ajufood/pedeai/model/PedidoModel.java
@@ -0,0 +1,48 @@
+package br.com.ajufood.pedeai.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="pedido")
+public class PedidoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotNull(message="A data e hora são obrigatórias")
+    @Column(name="dataHora", nullable=false)
+    private LocalDateTime dataHora;
+
+    @NotBlank(message="O status é obrigatório")
+    @Length(max=128)
+    @Column(name="status", nullable=false, length=128)
+    private String status;
+
+    @NotNull(message="O valor total é obrigatório")
+    @Column(name="valorTotal", nullable=false, precision=11, scale=2)
+    private BigDecimal valorTotal;
+
+    @NotNull(message="O cliente é obrigatório")
+    @ManyToOne
+    @JoinColumn(name="clienteID", nullable=false)
+    private ClienteModel cliente;
+
+    @NotNull(message="O endereço de entrega é obrigatório")
+    @ManyToOne
+    @JoinColumn(name="enderecoEntregaID", nullable=false)
+    private EnderecoModel enderecoEntrega;
+}
