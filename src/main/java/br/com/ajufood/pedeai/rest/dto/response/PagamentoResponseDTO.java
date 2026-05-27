bd527adbd9a6934bc327a59ca7b6a03222b3540a
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-05-27 15:50:59 -0300

    chore(pagamento): add construtores no ResponseDTO

--- src/main/java/br/com/ajufood/pedeai/rest/dto/response/PagamentoResponseDTO.java
+++ src/main/java/br/com/ajufood/pedeai/rest/dto/response/PagamentoResponseDTO.java
@@ -1,8 +1,17 @@
 package br.com.ajufood.pedeai.rest.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PagamentoResponseDTO {
     private int id;
     private BigDecimal valorPago;
