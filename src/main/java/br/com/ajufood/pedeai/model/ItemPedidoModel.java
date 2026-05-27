da99eb052676e0c6730a13292134e181a2f8ede9
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-05-26 23:17:24 -0300

    feat: add ItemPedidoModel

---
+++ src/main/java/br/com/ajufood/pedeai/model/ItemPedidoModel.java
@@ -0,0 +1,49 @@
+package br.com.ajufood.pedeai.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "itensPedido")
+public class ItemPedidoModel {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @NotNull(message="A quantidade é obrigatória")
+    @Positive(message="A quantidade deve ser positiva")
+    @Column(name="quantidade", nullable=false)
+    private Integer quantidade;
+
+    @NotNull(message="O preço unitário é obrigatório")
+    @Column(name="precoUnitario", nullable=false, precision=11, scale=2)
+    private BigDecimal precoUnitario;
+
+    @Column(name="subTotal", nullable=false, precision=11, scale=2)
+    private BigDecimal subTotal;
+
+    @NotNull(message="O pedido é obrigatório")
+    @ManyToOne
+    @JoinColumn(name="pedidoID", nullable=false)
+    private PedidoModel pedido;
+
+    @NotNull(message="O produto é obrigatório")
+    @ManyToOne
+    @JoinColumn(name="produtoID", nullable=false)
+    private ProdutoModel produto;
+
+}
