6c0adb3e7a6e1b5526e317d576c8afe92c709b06
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-05-27 15:49:29 -0300

    refactor(pagamento): Repository retorna lista de Pagamentos
    adequando relação 1:N de pedido e pagamentos

--- src/main/java/br/com/ajufood/pedeai/repository/PagamentoRepository.java
+++ src/main/java/br/com/ajufood/pedeai/repository/PagamentoRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<PagamentoModel, Integer> {
 
-    Optional<PagamentoModel> findByPedidoId(int pedidoId);
+    List<PagamentoModel> findByPedidoId(int pedidoId);
     List<PagamentoModel> findByFormaPagamentoId(int formaPagamentoId);
     boolean existsByPedidoId(int pedidoId);
 }
