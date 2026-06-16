284cfdf3938bebbe4187947c8707e9771f7a433e
Isabella Costa <166655665+isabellatsco@users.noreply.github.com>
2026-06-16 14:54:35 -0300

    feat(pedido): derived query methods como Page

--- src/main/java/br/com/ajufood/pedeai/repository/PedidoRepository.java
+++ src/main/java/br/com/ajufood/pedeai/repository/PedidoRepository.java
@@ -1,13 +1,17 @@
 package br.com.ajufood.pedeai.repository;
 
 import br.com.ajufood.pedeai.model.PedidoModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<PedidoModel, Integer> {
-    List<PedidoModel> findByClienteId(int clienteId);
+    Page<PedidoModel> findByClienteId(int clienteId, Pageable pageable);
+
+    Page<PedidoModel> findByClienteIdAndStatus(int clienteId, String status, Pageable pageable);
 }
